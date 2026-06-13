34f0667ff0c168584e4518acf31d79fcf46cd97d
Muhammad Afifi <adekmail31@gmail.com>
2026-06-13 17:49:43 +0700

    Integrate adaptive walking node into main task

--- main_task.py
+++ main_task.py
@@ -8,6 +8,7 @@ from .main_buttonhandler import ButtonSoccerNode
 from .main_head_control import HeadControl
 from .jarak import JarakCalculation
 from .kick_useless import KickDecision
+from .adaptive_walking import AdaptiveWalking
 # from .main_vision import VisionYolo
 
 
@@ -36,16 +37,17 @@ class MainTask(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    #vision main vision jangan taruh sini 
-    
+    # vision main vision jangan taruh sini
+
     main_node = MainTask()
     # bangun_node = FallRecoveryFull()
     button_node = ButtonSoccerNode()
     # head_control = HeadControl()
     # kick_control = KickDecision()
+    adaptive_walking = AdaptiveWalking()
 
     jarak = JarakCalculation()
-    
+
     executor = MultiThreadedExecutor()
     executor.add_node(main_node)
     # executor.add_node(head_control)
@@ -53,21 +55,24 @@ def main(args=None):
     # executor.add_node(bangun_node)
     executor.add_node(button_node)
     # executor.add_node(kick_control)
-    
+    executor.add_node(adaptive_walking)
+
     try:
         executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
         button_node.stop_all()
-        executor.shutdown()   
+        executor.shutdown()
         main_node.destroy_node()
         # head_control.destroy_node()
         jarak.destroy_node()
         # bangun_node.destroy_node()
         button_node.destroy_node()
         # kick_control.destroy_node()
+        adaptive_walking.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
